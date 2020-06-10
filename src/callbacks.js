40c92dbbab307ba6259c256ae71758d23538bdcb
Saul Florez <sflorez@distritohosting.com>
2020-06-10 19:25:41 -0400

    📦 NEW: Manejar peticiones con callbaks

---
+++ src/callbacks.js
@@ -0,0 +1,20 @@
+let XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+
+function fetchData(url_api, callback) {
+  let xhttp = new XMLHttpRequest();
+
+  xhttp.open("GET", url_api, true);
+
+  xhttp.onreadystatechange = function (event) {
+    if (xhttp.readyState === 4) {
+      if (xhttp.status === 200) {
+        callback(null, JSON.parse(xhttp.responseText));
+      } else {
+        const error = new Error("Error " + url_api);
+        return callback(error, null);
+      }
+    }
+  };
+
+  xhttp.send();
+}
