aa78bda068a5d442a6d2366aedd038d87294f718
Saul Florez <sflorez@distritohosting.com>
2020-06-11 13:08:22 -0400

    📦 NEW: Petición a una API usando callbacks

--- src/callbacks.js
+++ src/callbacks.js
@@ -1,4 +1,5 @@
 let XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+const API = "https://rickandmortyapi.com/api/character/";
 
 function fetchData(url_api, callback) {
   let xhttp = new XMLHttpRequest();
@@ -18,3 +19,16 @@ function fetchData(url_api, callback) {
 
   xhttp.send();
 }
+
+fetchData(API, function (error1, data1) {
+  if (error1) return console.error(error1);
+  fetchData(API + data1.results[0].id, function (error2, data2) {
+    if (error2) return console.error(error2);
+    fetchData(data2.origin.url, function (error3, data3) {
+      if (error3) return console.error(error3);
+      console.log(data1.info.count);
+      console.log(data2.name);
+      console.log(data3.dimension);
+    });
+  });
+});
