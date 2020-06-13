38ce50f294f918ce35f7d3bf8ac844eebd3f3304
Saul Florez <sflorez@distritohosting.com>
2020-06-12 20:16:34 -0400

    📦 NEW: Petición a una API usando promesas

--- src/promises.js
+++ src/promises.js
@@ -6,7 +6,7 @@ const fetchData = (url_api) => {
     let xhttp = new XMLHttpRequest();
     xhttp.open("GET", url_api, true);
 
-    xhttp.onreadystatechange = function (event) {
+    xhttp.onreadystatechange = (event) => {
       if (xhttp.readyState === 4) {
         xhttp.status === 200
           ? resolve(JSON.parse(xhttp.responseText))
@@ -17,3 +17,17 @@ const fetchData = (url_api) => {
     xhttp.send();
   });
 };
+
+fetchData(API)
+  .then((data) => {
+    console.log(data.info.count);
+    return fetchData(`${API}${data.results[0].id}`);
+  })
+  .then((data) => {
+    console.log(data.name);
+    return fetchData(data.origin.url);
+  })
+  .then((data) => {
+    console.log(data.dimension);
+  })
+  .catch((err) => console.log(err));
